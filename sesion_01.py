b66ac7422cd167d5c7caf65073fc3571609fe46f
jaretraymundo-sys <jaretraymundo@gmail.com>
2026-03-05 18:54:41 -0800

    guardado mi inicio en sesion phyton basico

---
+++ sesion_01.py
@@ -0,0 +1,13 @@
+#numerico y flotantes
+print(int(7))
+print(float(7.7))
+print(type(7))
+print(type(7.7))
+print(int(1+2))
+print(int(10*2))
+print("==Operadores Numericos==")
+# +
+# -
+# *
+# /
+# % Modulo
